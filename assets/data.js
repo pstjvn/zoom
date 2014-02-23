29532c367c5afd6049cb6f8bb6e9c54a5d0949d2
Peter StJ <regardingscot@gmail.com>
2014-02-23 16:10:37 +0200

    Add new data type, this time taken directly from the server.

--- assets/data.js
+++ assets/data.js
@@ -1,35 +1,13 @@
 var DATA = {
-  points: [{
-    id: 1,
-    x: 1,
-    y: 3,
-    s: 25,
-    c: '#fca',
-    bg: 'red'
-  },{
-    id: 2,
-    x: 100,
-    y: 3,
-    s: 15,
-    c: '#fca',
-    bg: 'green'
-  },{
-    id: 3,
-    x: 320,
-    y: 287,
-    s: 25,
-    c: '#fca',
-    bg: 'yellow'
-  }],
-  floor: {
-    id: 1,
-    w: 4000,
-    h: 4000,
-    imgsrc: 'assets/floor.jpg',
-    scale: 300
-  },
-  values: [{
-    min: 7,
-    max: 13
-  }]
+  points: [
+    {id:1,name:'one',x:1,y:3,s:25,c:'#fca',bg:'red'},
+    {id:2,name:'two',x:10,y:3,s:15,c:'#fca',bg:'green'},
+    {id:3,name:'threi',x:320,y:287,s:25,c:'#fca',bg:'yellow'}
+  ],
+  floor: {id:1,w:4000,h:4000,imgsrc:'assets/floor.jpg',scale:300},
+  values: [
+    {id:1,current:6,min:2,max:6},
+    {id:2,current:11,min:7,max:13},
+    {id:3,current:8,min:7,max:10}
+  ]
 };
